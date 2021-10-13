63a651f614aecbe5d79fc0235ac7b9a74f321945
Yurii-Shutkin <itorik@ukr.net>
2021-10-13 12:12:52 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let num = +prompt('Введите число');
+let flag = true;
+
+if (num <= 1) {
+    flag = false;
+}
+
+for (let i = 2; i < num; i ++) {
+    if (num % i === 0) {
+        flag = false;
+    }
+}
+
+console.log (`${num}; //${flag}`);
